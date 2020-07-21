4a35c546524725e347159500e141ce1953f48a78
Mateus Paiva Matiazzi <mateuspaivamatiazzi1@gmail.com>
2020-07-21 02:24:27 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@ export default {
   name: 'app',
   data () {
     return {
-      api_key: '',
+      api_key: 'fc19c359ec353b7d71a907a9924efcef',
       url_base: 'https://api.openweathermap.org/data/2.5/',
       query: '',
       weather: {}
